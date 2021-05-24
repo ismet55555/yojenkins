d37310173c6aec5c3d7fba4f8335356d6de1f4c4
Ismet Handzic <ismet.handzic@gmail.com>
2021-05-24 16:55:07 -0400

    Finished DockerJenkinsServer, some testing

---
+++ yo_jenkins/docker-python-class-test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import sys
+from pprint import pprint
+from time import time
+from Setup.DockerJenkinsServer import DockerJenkinsServer
+
+# Setup a message logging
+log_format = '[%(asctime)s] [%(relativeCreated)-4d] [%(filename)-23s:%(lineno)4s] %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=log_format, datefmt='%H:%M:%S')
+
+DJS = DockerJenkinsServer()
+DJS.setup()
+
+
