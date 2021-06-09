f29fe85ae2fc07147ed5b06cbf071f41fcc60099
Ismet Handzic <ismet.handzic@gmail.com>
2021-06-08 21:49:36 -0400

    Bump version: 0.0.18 → 0.0.19

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 from pprint import pprint
 
 # Package version number (Updated via bump2version tool)
-__version__ = "0.0.18"
+__version__ = "0.0.19"
 
 def get_requirements():
     """Load packages from requirements.txt"""
