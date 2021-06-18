beb778ab90c07010e9289aaf1abde8dc17922467
Ismet Handzic <ismet.handzic@gmail.com>
2021-06-17 22:20:37 -0400

    Bump version: 0.0.19 → 0.0.20

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pprint import pprint
 import setuptools
 
 # Package version number (Updated via bump2version tool)
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 
 def check_py_version():
     """Check the python version"""
