d59d107388b0b370ebe74c0ac9eb523e52a6d347
Ismet Handzic <ismet55555@users.noreply.github.com>
2022-05-24 23:10:58 -0400

    Bump packages (#122)
    * Bump version json2xml and pyinstaller
    
    * Bump version: 0.0.67 → 0.0.68

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 import setuptools
 
 # Package version number (Updated via bump2version tool)
-__version__ = "0.0.67"
+__version__ = "0.0.68"
 
 def check_py_version():
     """Check the python version"""
