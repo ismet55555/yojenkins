d2a43ef6c23dc6063776d735b3b01cb7facb3bb3
Ismet Handzic <ismet.handzic@gmail.com>
2022-06-13 19:10:27 -0400

    Bump version: 0.0.70 → 0.0.71

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 import setuptools
 
 # Package version number (Updated via bump2version tool)
-__version__ = "0.0.70"
+__version__ = "0.0.71"
 
 def check_py_version():
     """Check the python version"""
