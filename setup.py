3d67fdd7b547683063ce30d0763409e2721ee5c7
Ismet Handzic <ismet55555@users.noreply.github.com>
2022-09-13 00:18:45 -0400

    Chore: Update dependencies and bump version (#146)
    * Update dependencies
    
    * Bump version: 0.0.72 → 0.0.73

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 import setuptools
 
 # Package version number (Updated via bump2version tool)
-__version__ = "0.0.72"
+__version__ = "0.0.73"
 
 def check_py_version():
     """Check the python version"""
