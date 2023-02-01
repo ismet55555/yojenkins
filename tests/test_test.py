897c37ea5f01449a31edec669c7658f3036a24e8
Ismet Handzic <ismet.handzic@gmail.com>
2023-02-01 17:54:40 -0500

    initial work

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,6 +2,11 @@
 
 import logging
 
+from click.testing import CliRunner
+
+from yojenkins.__main__ import auth
+from yojenkins.cli_sub_commands.auth import show
+
 
 def test_success() -> None:
     """This is just a dummy test"""
@@ -9,7 +14,18 @@ def test_success() -> None:
     assert True
 
 
-def test_failure() -> None:
-    """This is just a dummy test"""
-    logging.info("This is just a dummy test that FAILS")
-    assert False
+# def test_failure() -> None:
+#     """This is just a dummy test"""
+#     logging.info("This is just a dummy test that FAILS")
+#     assert False
+
+def test_auth_show():
+    runner = CliRunner()
+    result = runner.invoke(show, [])
+    logging.fatal("TEST")
+    logging.fatal(result.exit_code)
+    logging.fatal(result.output)
+
+    assert result.exit_code == 0
+
+    
